8d773aefb59e2a62084c360f31a4cb88b33b1d99
Syed Abdullah <myselfsyedabdullah@gmail.com>
2025-08-21 18:01:05 +0500

    ! final register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,7 +44,7 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
 
 
 
